9fc2c90a5f4a77f04bd50f54f0a53dd523419793
m-pathania <mayankpmicky@gmail.com>
2022-01-26 19:48:40 +0530

    lazy and eager

--- 03_hibernate/13_eager_vs_lazy/src/main/java/com/hibernate_basics/one2many/LazyInstructorFail.java
+++ 03_hibernate/13_eager_vs_lazy/src/main/java/com/hibernate_basics/one2many/LazyInstructorFail.java
@@ -8,7 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class DeleteCourse {
+public class LazyInstructorFail {
     public static void main(String [] args){
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
 
@@ -16,22 +16,18 @@ public class DeleteCourse {
             Session session = factory.getCurrentSession();
             session.beginTransaction();
 
-            Course course = session.get(Course.class, 10);
-            System.out.println("\n\n\n\n" + course + "\n\n\n\n");
-
-            session.delete(course);
-
-            session.getTransaction().commit();
-
-            session = factory.getCurrentSession();
-            session.beginTransaction();
-
             Instructor inst1 = session.get(Instructor.class, 1);
             Instructor inst2 = session.get(Instructor.class, 2);
+            System.out.println("Instructors Loaded\n\n");
+            System.out.println(inst1.getFirstName()  + "\n\n");
+            System.out.println(inst2.getFirstName()  + "\n\n");
+            
+            session.getTransaction().commit();
+            session.close();
+            System.out.println("Session Closed\n\n");;
  
             System.out.println("\n\n\n\n" + inst1 + "\n\n" + inst2 + "\n\n\n\n");
 
-            session.getTransaction().commit();
         }finally{
             factory.close();
         }
