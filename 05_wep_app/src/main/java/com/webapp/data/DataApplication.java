9ac7468e89ad66f0f85a099a2179f6f0f5b44293
m-pathania <mayankpmicky@gmail.com>
2022-02-04 21:20:51 +0530

    starting hibernate project

--- 05_wep_app/src/main/java/com/webapp/data/DataApplication.java
+++ 05_wep_app/src/main/java/com/webapp/data/DataApplication.java
@@ -1,13 +1,9 @@
 package com.webapp.data;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataApplication.class, args);
+        System.out.println("\n\n\n\nTesting if java runs\n\n\n\n");
 	}
 
 }
